21b7e5d91706400f1c35f04057a38f4809d162ee
N-feng <308561157@qq.com>
2024-02-21 22:52:12 +0800

    feat✨: request must be second dev

--- .umirc.ts
+++ .umirc.ts
@@ -4,9 +4,16 @@ export default defineConfig({
   plugins: ['@umijs/plugins/dist/react-query'],
   reactQuery: {},
   routes: [
-    { path: "/", component: "index" },
-    { path: "/login", component: "login" },
-    { path: "/docs", component: "docs" },
+    {
+      path: '/',
+      component: '@/layouts/index',
+      routes: [
+        { path: "/", component: "index", name: "home" },
+        { path: "/docs", component: "docs", name: "docs" },
+        { path: "/products", component: "products", name: "products" },
+      ]
+    },
+    { path: "/login", component: "login", name: "login" },
   ],
   npmClient: 'pnpm',
 });
